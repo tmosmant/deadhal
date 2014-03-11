111e9adb8f905a2eb76a1ebab9810968f442ec8b
tmosmant <thomas.mosmant@gmail.com>
2014-03-11 11:33:55 +0100

    modification de la couleur de bordure des rooms dans la navigation

--- src/fr/upem/deadhal/graphics/drawable/NavigationLevelDrawable.java
+++ src/fr/upem/deadhal/graphics/drawable/NavigationLevelDrawable.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import fr.upem.deadhal.R;
 import fr.upem.deadhal.components.Corridor;
@@ -33,16 +34,15 @@ public class NavigationLevelDrawable extends AbstractLevelDrawable {
 		Room roomStart = m_levelHandler.getRoomStart();
 		Room roomEnd = m_levelHandler.getRoomEnd();
 		if (roomStart != null) {
-			drawRoomStart(canvas, roomStart);
+			drawSelectedRoom(canvas, roomStart, Paints.ROOM_START_BACKGROUND);
 		}
 		if (roomEnd != null) {
-			drawRoomEnd(canvas, roomEnd);
+			drawSelectedRoom(canvas, roomStart, Paints.ROOM_END_BACKGROUND);
 		}
 		drawLocalisation(canvas);
 
 		for (Corridor corridor : level.getCorridors().values()) {
-			if (m_levelHandler.getShortestPath().contains(
-					corridor.getId())) {
+			if (m_levelHandler.getShortestPath().contains(corridor.getId())) {
 				drawCorridor(canvas, corridor, Paints.CORRIDOR_HIGHLIGHT);
 			} else {
 				drawCorridor(canvas, corridor, Paints.CORRIDOR);
@@ -53,7 +53,9 @@ public class NavigationLevelDrawable extends AbstractLevelDrawable {
 	public void drawLocalisation(Canvas canvas) {
 		Room localisationRoom = m_levelHandler.getLocalisationRoom();
 		if (localisationRoom != null) {
-			canvas.drawBitmap(localisationBitmap(), m_levelHandler.getLocalisationX() - localisationBitmap().getWidth() / 2,
+			canvas.drawBitmap(localisationBitmap(),
+					m_levelHandler.getLocalisationX()
+							- localisationBitmap().getWidth() / 2,
 					m_levelHandler.getLocalisationY()
 							- localisationBitmap().getHeight() / 2,
 					Paints.LOCALISATION);
@@ -63,33 +65,21 @@ public class NavigationLevelDrawable extends AbstractLevelDrawable {
 	private Bitmap localisationBitmap() {
 		// no need to synchronize, this isn't static
 		if (m_localisationBitmap == null) {
-			m_localisationBitmap = BitmapFactory.decodeResource(
-					m_levelHandler.getView().getResources(),
+			m_localisationBitmap = BitmapFactory.decodeResource(m_levelHandler
+					.getView().getResources(),
 					R.drawable.ic_action_location_found);
 		}
 		return m_localisationBitmap;
 	}
 
-	private void drawRoomStart(Canvas canvas, Room room) {
+	private void drawSelectedRoom(Canvas canvas, Room room, Paint paint) {
 		float borderSize = (float) 1.5;
 
 		RectF rect = room.getRect();
 		RectF rectB = new RectF(rect.left + borderSize, rect.top + borderSize,
 				rect.right - borderSize, rect.bottom - borderSize);
-		canvas.drawRect(rect, Paints.ROOM_START_BACKGROUND);
-		canvas.drawRect(rectB, Paints.ROOM_SELECTED_BORDER);
+		canvas.drawRect(rect, paint);
+		canvas.drawRect(rectB, Paints.ROOM_BORDER);
 		drawTitle(canvas, room);
 	}
-
-	private void drawRoomEnd(Canvas canvas, Room room) {
-		float borderSize = (float) 1.5;
-
-		RectF rect = room.getRect();
-		RectF rectB = new RectF(rect.left + borderSize, rect.top + borderSize,
-				rect.right - borderSize, rect.bottom - borderSize);
-		canvas.drawRect(rect, Paints.ROOM_END_BACKGROUND);
-		canvas.drawRect(rectB, Paints.ROOM_SELECTED_BORDER);
-		drawTitle(canvas, room);
-	}
-
 }

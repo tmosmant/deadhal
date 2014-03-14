c31641e2103f0f671b03a877eea8efeaef14ead6
tmosmant <thomas.mosmant@gmail.com>
2014-03-14 02:19:48 +0100

    ajout de l'effet glissade du pointeur dans la navigation

--- src/fr/upem/deadhal/fragments/listeners/NavigationSensorEventListener.java
+++ src/fr/upem/deadhal/fragments/listeners/NavigationSensorEventListener.java
@@ -18,32 +18,37 @@ public class NavigationSensorEventListener implements SensorEventListener {
 		this.m_levelHandler = m_levelHandler;
 	}
 
+	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 	}
 
+	@Override
 	public void onSensorChanged(SensorEvent event) {
-		float x = 0, y = 0;
-		switch (activity.getWindowManager().getDefaultDisplay().getRotation()) {
-		case Surface.ROTATION_0:
-			x = -event.values[0];
-			y = event.values[1];
-			break;
-		case Surface.ROTATION_90:
-			x = event.values[1];
-			y = event.values[0];
-			break;
-		case Surface.ROTATION_180:
-			x = event.values[0];
-			y = -event.values[1];
-			break;
-		case Surface.ROTATION_270:
-			x = -event.values[1];
-			y = -event.values[0];
-			break;
+		if (m_levelHandler.getLocalisationRoom() != null) {
+			float x = 0, y = 0;
+			switch (activity.getWindowManager().getDefaultDisplay()
+					.getRotation()) {
+			case Surface.ROTATION_0:
+				x = -event.values[0];
+				y = event.values[1];
+				break;
+			case Surface.ROTATION_90:
+				x = event.values[1];
+				y = event.values[0];
+				break;
+			case Surface.ROTATION_180:
+				x = event.values[0];
+				y = -event.values[1];
+				break;
+			case Surface.ROTATION_270:
+				x = -event.values[1];
+				y = -event.values[0];
+				break;
+			}
+			x += m_levelHandler.getLocalisationX();
+			y += m_levelHandler.getLocalisationY();
+			m_levelHandler.moveWithSensor(x, y);
 		}
-		x += m_levelHandler.getLocalisationX();
-		y += m_levelHandler.getLocalisationY();
-		m_levelHandler.move(x, y);
 	}
 
 }

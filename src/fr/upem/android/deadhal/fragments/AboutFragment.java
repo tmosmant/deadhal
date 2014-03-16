99ddd52508c6e3721483e11305849e22aa0c4429
Michael Remy <remy.mick@gmail.com>
2014-03-16 18:47:48 +0100

    fragment about

--- src/fr/upem/android/deadhal/fragments/AboutFragment.java
+++ src/fr/upem/android/deadhal/fragments/AboutFragment.java
@@ -1,10 +1,15 @@
 package fr.upem.android.deadhal.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import fr.upem.deadhal.R;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AboutFragment extends AbstractFragment {
 
@@ -19,6 +24,52 @@ public class AboutFragment extends AbstractFragment {
 		View rootView = inflater.inflate(R.layout.fragment_about, container,
 				false);
 		getActivity().setTitle(R.string.about);
+
+		ImageView imageView = (ImageView) rootView.findViewById(R.id.thomas);
+		try {
+			InputStream ims = getActivity().getAssets().open("thomas.jpg");
+			// load image as Drawable
+			Drawable d = Drawable.createFromStream(ims, null);
+			// set image to ImageView
+			imageView.setImageDrawable(d);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		imageView = (ImageView) rootView.findViewById(R.id.michael);
+		try {
+			InputStream ims = getActivity().getAssets().open("michael.jpg");
+			// load image as Drawable
+			Drawable d = Drawable.createFromStream(ims, null);
+			// set image to ImageView
+			imageView.setImageDrawable(d);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		imageView = (ImageView) rootView.findViewById(R.id.vincent);
+		try {
+			InputStream ims = getActivity().getAssets().open("vincent.jpg");
+			// load image as Drawable
+			Drawable d = Drawable.createFromStream(ims, null);
+			// set image to ImageView
+			imageView.setImageDrawable(d);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		imageView = (ImageView) rootView.findViewById(R.id.fazal);
+		try {
+			InputStream ims = getActivity().getAssets().open("fazal.jpg");
+			// load image as Drawable
+			Drawable d = Drawable.createFromStream(ims, null);
+			// set image to ImageView
+			imageView.setImageDrawable(d);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 		return rootView;
 	}
 

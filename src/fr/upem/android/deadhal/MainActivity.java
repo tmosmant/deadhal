695dfaf51f9c306bb3110685cde02ea9282af0ab
Michael Remy <remy.mick@gmail.com>
2014-03-16 17:42:53 +0100

    modif largeur ecran help et suppression log inutile

--- src/fr/upem/android/deadhal/MainActivity.java
+++ src/fr/upem/android/deadhal/MainActivity.java
@@ -1,8 +1,5 @@
 package fr.upem.android.deadhal;
 
-import java.io.File;
-import java.util.ArrayList;
-
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Context;
@@ -13,12 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -26,18 +18,13 @@ import fr.upem.android.deadhal.components.Level;
 import fr.upem.android.deadhal.drawers.adapters.DrawerMainListAdapter;
 import fr.upem.android.deadhal.drawers.listeners.DrawerMainListener;
 import fr.upem.android.deadhal.drawers.models.DrawerMainItem;
-import fr.upem.android.deadhal.fragments.AboutFragment;
-import fr.upem.android.deadhal.fragments.AbstractFragment;
-import fr.upem.android.deadhal.fragments.EditionCorridorFragment;
-import fr.upem.android.deadhal.fragments.EditionFragment;
-import fr.upem.android.deadhal.fragments.FragmentType;
-import fr.upem.android.deadhal.fragments.HelpFragment;
-import fr.upem.android.deadhal.fragments.NavigationFragment;
-import fr.upem.android.deadhal.fragments.OpenFragment;
-import fr.upem.android.deadhal.fragments.SaveFragment;
+import fr.upem.android.deadhal.fragments.*;
 import fr.upem.android.deadhal.tasks.OpenTask;
 import fr.upem.android.deadhal.utils.Storage;
 import fr.upem.deadhal.R;
+
+import java.io.File;
+import java.util.ArrayList;
 
 /**
  * Main activity of the deadhal project.
@@ -64,10 +51,6 @@ public class MainActivity extends Activity implements DrawerMainListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState != null) {
-			Log.e("savedInstance", "not null");
-		}
 
 		if (savedInstanceState == null && getIntent().getData() != null) {
 			Uri uri = getIntent().getData();
